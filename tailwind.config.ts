84179bf035eef1c968a26b299de1f919212c0021
Lucas Sunsi Abreu <lsunsi@pm.me>
2023-12-24 08:25:53 -0300

    chore: add skeleton and tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+export default {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [skeleton({ themes: { preset: ["hamlindigo"] } })]
+} satisfies Config;
