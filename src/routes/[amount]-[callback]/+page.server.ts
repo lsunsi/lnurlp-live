6574f7928be0104414c4be8bc4a25768084053bf
Lucas Sunsi Abreu <lsunsi@pm.me>
2023-12-25 08:29:32 -0300

    feat: add timer and satoshis to callback view

--- src/routes/[amount]-[callback]/+page.server.ts
+++ src/routes/[amount]-[callback]/+page.server.ts
@@ -1,4 +1,8 @@
 import { callback } from "$lib";
 import type { PageServerLoad } from "./$types";
+import bolt11 from "bolt11";
 
-export const load: PageServerLoad = ({ params }) => callback(atob(params.callback), +params.amount * 1000);
+export const load: PageServerLoad = async ({ params }) => {
+	const c = await callback(atob(params.callback), +params.amount * 1000);
+	return { ...c, decoded: bolt11.decode(c.pr) }
+};
--- src/routes/[amount]-[callback]/+page.svelte
+++ src/routes/[amount]-[callback]/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
+
+	const expiresAt = data.decoded.timeExpireDate && new Date(data.decoded.timeExpireDate * 1000);
+	let expiresIn = expiresAt && Math.trunc((+expiresAt - +new Date()) / 1000);
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			expiresIn = expiresAt && Math.max(Math.trunc((+expiresAt - +new Date()) / 1000), 0);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	});
 </script>
+
+<div class="flex place-content-center gap-x-2">
+	<span class="variant-ghost badge">{data.decoded.satoshis} satoshis</span>
+	{#if expiresIn && expiresIn > 0}
+		<span class="variant-ghost badge">Expires in {expiresIn} seconds</span>
+	{:else if expiresIn === 0}
+		<span class="variant-filled-error badge">Expired</span>
+	{/if}
+</div>
 
 <qr-code class="visible max-h-96 max-w-96" contents={`lightning:${data.pr}`} />
